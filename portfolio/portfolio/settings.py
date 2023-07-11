62b065932214ba9d7ee675d1d8cf6ca07d9ef328
anush.23 <anushitik@gmail.com>
2023-07-12 00:22:04 +0400

    change in settings

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-_1wy^lnjkny=m)o(up-$!2kv__b^busk_85+r)42_$czridj&9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['anush23.pythonanywhere.com']
 
 
 # Application definition

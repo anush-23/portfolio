2002ddc2b174bd571acedef8d2771dd3349d053e
anush.23 <anushitik@gmail.com>
2023-07-12 00:26:31 +0400

    change in urls

--- portfolio/resume/urls.py
+++ portfolio/resume/urls.py
@@ -5,7 +5,6 @@ from .views import *
 
 urlpatterns = [
     path('', home, name='home'),
-    path('home/', home, name='home'),
     path('resume/', resume, name='resume'),
     path('projects/', projects, name='projects'),
     path('contact/', contact, name='contact'),
